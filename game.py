e9ee4136794795c9c3f53687456962f42babe213
fef29 <ferran.ffv@gmail.com>
2022-03-25 00:47:32 +0100

    [NEW] Refactor and game over logic

---
+++ game.py
@@ -0,0 +1,95 @@
+import pygame
+import time
+from pygame.locals import *
+from constants import *
+from aaple import Apple
+from snake import Snake
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((1000, 600))
+        self.surface.fill(BACKGROUND_COLOR)
+        self.snake = Snake(self.surface, 2)
+        self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def is_collision(self, x1, y1, x2, y2):
+        if x2 <= x1 < x2 + SIZE:
+            if y2 <= y1 < y2 + SIZE:
+                return True
+        return False
+
+    def display_score(self):
+        font = pygame.font.SysFont("arial", 30)
+        score = font.render(f"Score: {self.snake.length}", True, (255, 255, 255))
+        self.surface.blit(score, (850, 10))
+
+    def show_game_over(self):
+        self.surface.fill(BACKGROUND_COLOR)
+        font = pygame.font.SysFont("arial", 30)
+        line1 = font.render(f"Score: {self.snake.length}", True, (255, 255, 255))
+        self.surface.blit(line1, (200, 300))
+        line2 = font.render("To play again press Enter. To exit press Escape!", True, (255, 255, 255))
+        self.surface.blit(line2, (200, 350))
+        pygame.display.flip()
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+
+        # snake colliding with apple
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+
+        # snake colliding with itself
+        for i in range(3, self.snake.length):
+            if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+                raise "Game over"
+
+    def reset(self):
+        self.snake = Snake(self.surface, 2)
+        self.apple = Apple(self.surface)
+
+    def run(self):
+        running = True
+        pause = False
+
+        while running:  # infinite loop
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+
+                    if event.key == K_RETURN:
+                        pause = False
+
+                    if not pause:
+                        if event.key == K_UP:
+                            self.snake.move_up()
+
+                        if event.key == K_DOWN:
+                            self.snake.move_down()
+
+                        if event.key == K_LEFT:
+                            self.snake.move_left()
+
+                        if event.key == K_RIGHT:
+                            self.snake.move_right()
+
+                elif event.type == QUIT:
+                    running = False
+            try:
+                if not pause:
+                    self.play()
+            except Exception as e:
+                self.show_game_over()
+                pause = True
+                self.reset()
+            time.sleep(0.3)
+
